419bd037f5f17d3142ee054e48aae8e454ab0161
Autumn Apsey <aapsey@nexient.com>
2018-01-21 12:09:48 -0500

    Added check to prevent duplicate entrants to list

--- commands/add.js
+++ commands/add.js
@@ -3,12 +3,14 @@ const config = require("../config.json");
 
 exports.run = (client, message, [...name]) => {
     const leadership = message.member.roles.some(r=>["Leader", "Awficer", "Junior Awficer", "Owner"].includes(r.name));
+    const entrant = name.join(" ");
+    let listCheck = config.entrants.map(entrant => entrant.toLowerCase());
 
     if (!leadership) {
         message.channel.send(`You're not permitted to use this command.`);
+    } else if (listCheck.filter(entrant => entrant.toLowerCase())){
+        message.channel.send(`${entrant} is already on the list.`);
     } else {
-
-        let entrant = name.join(" ");
 
         let newList = [...config.entrants, entrant];
 
